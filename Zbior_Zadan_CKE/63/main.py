6acf48b46cc90e3a11029094ee192e2ea6ea4347
szche <github@chadam.pl>
2019-03-14 15:24:14 +0100

    z.63 CKE

--- Zbior_Zadan_CKE/63/main.py
+++ Zbior_Zadan_CKE/63/main.py
@@ -11,15 +11,44 @@ def czy_dwucykliczny(ciag):
     else:
         return False
 
+def is_prime(liczba):
+    prime = True
+    if liczba == 1:
+        return False
+    for i in range(2, (liczba//2)+1):
+        if liczba%i == 0:
+            prime = False
+            break
+    return prime
+
+def czy_popierwsza(liczba, dzielniki):
+    for dzielnik1 in dzielniki:
+        for dzielnik2 in dzielniki:
+
+            if dzielnik1 * dzielnik2 == liczba:
+                return True
+    return False
+
+def znajdz_pierwsze_dzielniki(liczba):
+    #print(liczba)
+    dzielniki = []
+    for i in range(1, liczba+1):
+        if liczba%i == 0 and is_prime(i) == True:
+            dzielniki.append(i)
+    #print(liczba, dzielniki)
+    return czy_popierwsza(liczba, dzielniki)
 
 dwucykliczne = []
 bez2jedynek = []
+liczby_polpierwsze = []
 for item in wszystko:
     if czy_dwucykliczny(item):
         dwucykliczne.append(item)
     if "11" not in item:
         bez2jedynek.append(item)
-    czy_polpierwsza(int(item, 2))
+    if znajdz_pierwsze_dzielniki(int(item, 2)):
+        liczby_polpierwsze.append(int(item, 2))
+
        
 print("-" * 40)
 print("Istnieje {} ciagow dwucyklicznych: ".format(len(dwucykliczne)))
@@ -28,3 +57,7 @@ print("-" * 40)
 
 print("{} ciagow nie posiada sasiadujacych jedynek".format(len(bez2jedynek)))
 print("-" * 40)
+
+print("{} ciagow sa polpierwsze, najwieksza z nich to {}, a najmniejsza to {}".format(len(liczby_polpierwsze), max(liczby_polpierwsze), min(liczby_polpierwsze)))
+print("-" * 40)
+
