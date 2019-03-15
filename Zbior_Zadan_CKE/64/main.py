0189d35b69259a76b1542e0d71f34b043ef140cc
szche <github@chadam.pl>
2019-03-15 16:08:24 +0100

    z.64 CKE 3)

--- Zbior_Zadan_CKE/64/main.py
+++ Zbior_Zadan_CKE/64/main.py
@@ -39,10 +39,37 @@ def wyswietl(obraz):
     for line in obraz:
         print(line)
 
+def wyznacz_pion(obraz, indeks):
+    pion = [line[indeks] for line in obraz]
+    return "".join(pion)
+
+
+def sprawdz_poprawnosc(obraz):
+    bezRamki = usun_ramke(obraz)
+    for x, poziom in enumerate(bezRamki):
+        poziomBit = poziom.count("1")%2
+        ramkaPoziom = int(obraz[x][-1])
+        jestBladPoziom = False
+        jestBladPion = False
+        for y, pion in enumerate(poziom):
+            pionBit = wyznacz_pion(bezRamki, y).count("1")%2
+            ramkaPion = int(obraz[-1][y])
+            if poziomBit != ramkaPoziom and pionBit != ramkaPion:
+                return "nienaprawialne"
+            if poziomBit != ramkaPoziom:
+                jestBladPoziom = True
+            if pionBit != ramkaPion:
+                jestBladPion = True
+    if jestBladPion or jestBladPoziom:
+        return "naprawialne"
+    return "poprawne"
 
 rewersy = []
 najwiecej_czarnych = 0
 rekurencyjne = []
+poprawne = 0
+naprawialne = 0
+nienaprawialne = 0
 for x, obraz in enumerate(wszystko):
     sprawdzRewers = czy_rewers(usun_ramke(obraz))
     if sprawdzRewers[0]:
@@ -51,9 +78,16 @@ for x, obraz in enumerate(wszystko):
             najwiecej_czarnych = sprawdzRewers[1]
     if sprawdz_rekurencje(usun_ramke(obraz)):
         rekurencyjne.append(obraz)
-
+    if sprawdz_poprawnosc(obraz) == "nienaprawialne":
+        nienaprawialne += 1
+    elif sprawdz_poprawnosc(obraz) == "naprawialne":
+        naprawialne += 1
+    elif sprawdz_poprawnosc(obraz) == "poprawne":
+        poprawne += 1
 print('-' * 40)
 print("Jest {} rewersow, najwieksza liczba pikseli czarnych to {}".format(len(rewersy), najwiecej_czarnych))
 print('-' * 40)
 print("Jest {} obrazow rekurencyjnych, pierwszy z nich to: ".format(len(rekurencyjne)))
 wyswietl(usun_ramke(rekurencyjne[0]))
+print('-' * 40)
+print(nienaprawialne,naprawialne, poprawne)
